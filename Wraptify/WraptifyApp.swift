940878ddeef247e1fd86c7f182b3c24b43bc1753
sam1rc <samir.colpas@hotmail.com>
2023-09-20 14:22:48 -0500

    first commit

---
+++ Wraptify/WraptifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WraptifyApp.swift
+//  Wraptify
+//
+//  Created by Samir Colpas on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WraptifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
